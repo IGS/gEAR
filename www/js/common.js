21698397be4ad2ce2941ee689b691c53b1e1de4e
Joshua Orvis <jorvis@gmail.com>
2021-09-01 10:15:06 -0500

    Moved copyToClipboard function to common.js

--- www/js/common.js
+++ www/js/common.js
@@ -157,6 +157,29 @@ function common_datetime() {
     return date + ' ' + time;
 }
 
+function copyToClipboard(text) {
+    // https://stackoverflow.com/a/59594066
+    if (window.clipboardData && window.clipboardData.setData) {
+        // IE specific code path to prevent textarea being shown while dialog is visible.
+        return clipboardData.setData("Text", text); 
+
+    } else if (document.queryCommandSupported && document.queryCommandSupported("copy")) {
+        var textarea = document.createElement("textarea");
+        textarea.textContent = text;
+        textarea.style.position = "fixed";  // Prevent scrolling to bottom of page in MS Edge.
+        document.body.appendChild(textarea);
+        textarea.select();
+        try {
+            return document.execCommand("copy");  // Security exception may be thrown by some browsers.
+        } catch (ex) {
+            console.warn("Copy to clipboard failed.", ex);
+            return false;
+        } finally {
+            document.body.removeChild(textarea);
+        }
+    }
+}
+
 $('#navigation_bar').on('click', '#btn_sign_in', function(e){
     // Reset the appearance in case there was a previous login error
     $('#user_email').css({ 'color':'black', 'font-weight':'normal' });
