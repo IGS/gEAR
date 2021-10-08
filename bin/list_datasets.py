0d42c8d2e0292498abe2103c10c138f000ea0b88
Joshua Orvis <jorvis@gmail.com>
2021-10-08 12:26:58 -0500

    Added profile name to output as first column (issue #60)

--- bin/list_datasets.py
+++ bin/list_datasets.py
@@ -24,7 +24,7 @@ def main():
     qry = """
           SELECT d.id, d.title, d.dtype, g.email, d.pubmed_id, d.geo_id, d.is_public, d.date_added
             FROM dataset d
-                 JOIN guser g on d.owner_id=g.id
+                 JOIN guser g ON d.owner_id=g.id
            WHERE d.marked_for_removal = 0
     """
     qry_args = []
@@ -39,11 +39,36 @@ def main():
 
     for (dataset_id, title, dtype, email, pubmed_id, geo_id, is_public, date_added) in cursor:
         visibility = 'Public' if is_public else 'Private'
-        print("{0}\t{1}\t{2}\t{3}\t{4}\t{5}\t{6}\t{7}".format(dataset_id, title, dtype, email, pubmed_id, geo_id, visibility, date_added))
+        layout_str = get_layout_string(cnx, dataset_id)
+        print("{0}\t{1}\t{2}\t{3}\t{4}\t{5}\t{6}\t{7}\t{8}".format(layout_str, dataset_id, title, dtype, email, pubmed_id, geo_id, visibility, date_added))
 
     cursor.close()
     cnx.close()
 
+def get_layout_string(cnx, dataset_id):
+    cursor = cnx.get_cursor()
+    labels = list()
+
+    qry = """
+          SELECT l.share_id, l.label
+            FROM layout l 
+                 JOIN layout_members lm ON lm.layout_id=l.id
+           WHERE lm.dataset_id = %s
+    """
+    cursor.execute(qry, [dataset_id,])
+
+    for (share_id, label) in cursor:
+        labels.append(label)
+
+    cursor.close()
+
+    if len(labels):
+        return " / ".join(labels)
+    else:
+        return "None"
+
+    
+    
 if __name__ == '__main__':
     main()
     
