dbf27868899a41b22a08ae6b2c63f66cc4c0fd1f
Joshua Orvis <jorvis@gmail.com>
2021-11-10 11:07:11 -0600

    Added framework for running all testing scripts and displaying results

--- tests/accounts__create_account.py
+++ tests/accounts__create_account.py
@@ -13,11 +13,11 @@ This testing script performs the following actions:
 Future things to test
 
 - Test error handling if passwords don't match
+- Test error handling if email already exists
 
 """
 
 from selenium import webdriver
-browser = webdriver.Chrome()
 import configparser
 import time
 
@@ -25,50 +25,57 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 
-config = configparser.ConfigParser()
-config.read('../gear.ini')
+def main():
+    browser = webdriver.Chrome()
+    results = list()
+    
+    config = configparser.ConfigParser()
+    config.read('../gear.ini')
 
-browser = webdriver.Chrome()
-browser.get(config['test']['host'])
+    browser = webdriver.Chrome()
+    browser.get(config['test']['host'])
 
-create_acct_link = browser.find_element(By.ID, 'create_acct_link')
-create_acct_link.click()
+    create_acct_link = browser.find_element(By.ID, 'create_acct_link')
+    create_acct_link.click()
 
-name_box = browser.find_element(By.ID, 'inputName')
-name_box.send_keys(config['test']['user_name'])
+    name_box = browser.find_element(By.ID, 'inputName')
+    name_box.send_keys(config['test']['user_name'])
 
-inst_box = browser.find_element(By.ID, 'inputInstitution')
-inst_box.send_keys(config['test']['user_institution'])
+    inst_box = browser.find_element(By.ID, 'inputInstitution')
+    inst_box.send_keys(config['test']['user_institution'])
 
-pass_box = browser.find_element(By.ID, 'inputPassword')
-pass_box.send_keys(config['test']['password'])
+    pass_box = browser.find_element(By.ID, 'inputPassword')
+    pass_box.send_keys(config['test']['password'])
 
-repass_box = browser.find_element(By.ID, 'retypePassword')
-repass_box.send_keys(config['test']['password'])
+    repass_box = browser.find_element(By.ID, 'retypePassword')
+    repass_box.send_keys(config['test']['password'])
 
-# skip required e-mail and test for error
+    # skip required e-mail and test for error
 
-submit_box = browser.find_element(By.ID, 'btn_account_creation_submit')
-submit_box.click()
+    submit_box = browser.find_element(By.ID, 'btn_account_creation_submit')
+    submit_box.click()
 
-email_warning =  browser.find_element(By.ID, 'email_invalid')
+    email_warning =  browser.find_element(By.ID, 'email_invalid')
 
-if email_warning.is_displayed():
-    print("Initial E-mail wasn't provided")
-else:
-    print("Initial E-mail was provided")
+    if email_warning.is_displayed():
+        results.append({"success": 1, "label": "Missing input validation"})
+    else:
+        results.append({"success": 0, "label": "Missing input validation"})
 
-email_box = browser.find_element(By.ID, 'inputEmail')
-email_box.send_keys(config['test']['user_email'])
+    email_box = browser.find_element(By.ID, 'inputEmail')
+    email_box.send_keys(config['test']['user_email'])
 
-submit_box.click()
+    submit_box.click()
 
-if email_warning.is_displayed():
-    print("Secondary E-mail wasn't provided")
-else:
-    print("Secondary E-mail was provided")
+    if email_warning.is_displayed():
+        results.append({"success": 0, "label": "Account creation"})
+    else:
+        results.append({"success": 1, "label": "Account creation"})
 
+    time.sleep(2)
+    browser.close()
 
+    return results
 
-time.sleep(2)
-browser.close()
+if __name__ == '__main__':
+    main()
