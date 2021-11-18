be7bfa5c8ea7c3902b9449e274544bc0f0317d82
Joshua Orvis <jorvis@gmail.com>
2021-11-18 09:31:48 -0600

    Made curator test scripts executable

--- tests/index__primary_search__single_gene.py
+++ tests/index__primary_search__single_gene.py
@@ -1,6 +1,8 @@
 #!/opt/bin/python3
 
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+
 browser = webdriver.Chrome()
 
 from selenium import webdriver
@@ -9,9 +11,9 @@ from selenium.webdriver.common.keys import Keys
 browser = webdriver.Chrome()
 browser.get('https://umgear.org/')
 
-search_box = browser.find_element_by_id('search_gene_symbol_intro')
+search_box = browser.find_element(By.ID, 'search_gene_symbol_intro')
 search_box.send_keys('Sox2')
 
-exact_match = browser.find_element_by_id('exact_match_icon')
+exact_match = browser.find_element(By.ID, 'exact_match_icon')
 exact_match.click()
 
