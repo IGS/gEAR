6538bb835f229ea680b16483f32e4348209f17ce
Shaun Adkins <sadkins@som.umaryland.edu>
2021-11-22 08:37:51 -0500

    changing gene cart icon to shopping cart

--- www/js/classes/tree.js
+++ www/js/classes/tree.js
@@ -118,7 +118,7 @@ class GeneCartTree extends Tree {
                         'icon': 'fa fa-folder-o'
                     },
                     'gene': {
-                        'icon': 'fa fa-random',
+                        'icon': 'fa fa-shopping-cart',
                         'valid_children':[]
                     }
                 }
