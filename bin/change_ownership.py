bb83d10f8ef6a9b4e66402a5707cf592da6c1d3b
Joshua Orvis <jorvis@gmail.com>
2021-12-13 19:46:05 -0600

    Changed lib path import method so utility scripts could be run from anywhere

--- bin/change_ownership.py
+++ bin/change_ownership.py
@@ -13,7 +13,7 @@ import argparse
 import os
 import sys
 
-lib_path = os.path.abspath(os.path.join('..', 'lib'))
+lib_path = os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', 'lib'))
 sys.path.append(lib_path)
 import geardb
 
