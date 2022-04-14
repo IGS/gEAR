948af656584980cc0430af5c1a673360ba4ab47a
Joshua Orvis <jorvis@gmail.com>
2022-04-14 13:53:30 -0500

    Added ServerConfig class to abstract it from the db module

---
+++ lib/gear/serverconfig.py
@@ -0,0 +1,18 @@
+import os
+import sys
+import configparser
+
+class ServerConfig:
+    """
+    Provides the parsed version of the gear.ini file
+
+    Returns: a configparser.ConfigParser() object
+    """
+    def __init__(self):
+        self.config = None
+
+    def parse(self):
+        ini_path = "{0}/../../gear.ini".format(os.path.dirname(__file__))
+        self.config = configparser.ConfigParser()
+        self.config.read(ini_path)
+        return self.config
