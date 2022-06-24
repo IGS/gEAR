bd90a7828e050ab513405fcf72e47a1fef9936ad
adkinsrs <sadkins@som.umaryland.edu>
2022-06-24 09:53:22 -0400

    mirroring changes in 62ba899e0dd398ae50051593bc2f026ac0eb1f62

--- www/js/index.js
+++ www/js/index.js
@@ -1209,6 +1209,11 @@ function add_state_history(searched_entities, projection_source=null, is_pca=nul
     let state_url = "/index.html?"
         + `multigene_plots=${state_info.multigene_plots}`;
 
+     // Currently dataset share id and layout id URL params are mutually exclusive (or neither is used)
+     if (dataset_id) {
+        state_info.share_id = dataset_id;
+        state_url += `&share_id=${dataset_id}`;
+    }
 
     if (layout_id) {
         state_info.layout_id = layout_id;
