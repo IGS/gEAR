bc4f466e6ccd15506a6ce1af4b6ea3eb271e4b81
adkinsrs <sadkins@som.umaryland.edu>
2022-12-07 11:18:49 -0500

    removing plt.clf()

--- www/api/resources/tsne_data.py
+++ www/api/resources/tsne_data.py
@@ -432,8 +432,7 @@ class TSNEData(Resource):
         io_fig.tight_layout()   # This crops out much of the whitespace around the plot. The next line does this with the legend too
         io_fig.savefig(io_pic, format='png', bbox_inches="tight")
         io_pic.seek(0)
-        sleep(2)    # Give enough time for the IOStream to save (had white plots in the past)
-        plt.clf()   # Clear current fig
+        #sleep(2)    # Give enough time for the IOStream to save (had white plots in the past)
         plt.close()  # Prevent zombie plots, which can cause issues
 
         return {
