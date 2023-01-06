IGS/gEAR

commit ec9d890213f8b04cdd9ab5d55542848ad5d929f6

Closing adata object before converting projection to adata

www/api/resources/multigene_dash_data.py
@@ -110,6 +110,9 @@ def create_projection_adata(dataset_adata, dataset_id, projection_id):
     except:
         raise PlotError("Could not create projection AnnData object from CSV.")
     projection_adata.obs = dataset_adata.obs
+    # Close dataset adata so that we do not have a stale opened object
+    if dataset_adata.isbacked:
+        dataset_adata.file.close()
     projection_adata.var["gene_symbol"] = projection_adata.var_names
     # Associate with a filename to ensure AnnData is read in "backed" mode
     projection_adata.filename = projection_adata_path
@@ -186,7 +189,7 @@ class MultigeneDashData(Resource):
             }
 
         # Using adata with "backed" mode does not work with volcano plot
-        adata = ana.get_adata(backed=False)
+        adata = ana.get_adata(backed=True)
 
         adata.obs = order_by_time_point(adata.obs)
 

www/api/resources/plotly_data.py
@@ -36,6 +36,9 @@ def create_projection_adata(dataset_adata, dataset_id, projection_id):
     except:
         raise PlotError("Could not create projection AnnData object from CSV.")
     projection_adata.obs = dataset_adata.obs
+    # Close dataset adata so that we do not have a stale opened object
+    if dataset_adata.isbacked:
+        dataset_adata.file.close()
     projection_adata.var["gene_symbol"] = projection_adata.var_names
     # Associate with a filename to ensure AnnData is read in "backed" mode
     projection_adata.filename = projection_adata_path

www/api/resources/svg_data.py
@@ -32,6 +32,9 @@ def create_projection_adata(dataset_adata, dataset_id, projection_id):
     except:
         raise PlotError("Could not create projection AnnData object from CSV.")
     projection_adata.obs = dataset_adata.obs
+    # Close dataset adata so that we do not have a stale opened object
+    if dataset_adata.isbacked:
+        dataset_adata.file.close()
     projection_adata.var["gene_symbol"] = projection_adata.var_names
     # Associate with a filename to ensure AnnData is read in "backed" mode
     projection_adata.filename = projection_adata_path

www/api/resources/tsne_data.py
@@ -100,9 +100,13 @@ def create_projection_adata(dataset_adata, dataset_id, projection_id):
     except:
         raise PlotError("Could not create projection AnnData object from CSV.")
     projection_adata.obs = dataset_adata.obs
+    # Close dataset adata so that we do not have a stale opened object
+    if dataset_adata.isbacked:
+        dataset_adata.file.close()
     projection_adata.var["gene_symbol"] = projection_adata.var_names
     # Associate with a filename to ensure AnnData is read in "backed" mode
     projection_adata.filename = projection_adata_path
+
     return projection_adata
 
 def get_colorblind_scale(n_colors):

commit 9b80b242f8498de2f17534e00e899af4c310cd2c

fixing bug introduced in ec9d890213f8b04cdd9ab5d55542848ad5d929f6

www/api/resources/multigene_dash_data.py
@@ -107,7 +107,9 @@ def create_projection_adata(dataset_adata, dataset_id, projection_id):
     projection_csv_path = projection_dir.joinpath("{}.csv".format(projection_id))
     try:
         projection_adata = sc.read_csv(projection_csv_path)
-    except:
+    except Exception as e:
+        import sys
+        print(str(e), file=sys.stderr)
         raise PlotError("Could not create projection AnnData object from CSV.")
     projection_adata.obs = dataset_adata.obs
     # Close dataset adata so that we do not have a stale opened object
@@ -189,7 +191,7 @@ class MultigeneDashData(Resource):
             }
 
         # Using adata with "backed" mode does not work with volcano plot
-        adata = ana.get_adata(backed=True)
+        adata = ana.get_adata(backed=False)
 
         adata.obs = order_by_time_point(adata.obs)
 