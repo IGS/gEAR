f967a4789fb9c0c7e635b5cc4ee969a109868fdf
Joshua Orvis <jorvis@gmail.com>
2023-01-20 14:45:21 -0600

    Registration and unregistration currently works. Needs polishing and waitlist support still

--- www/cgi/set_user_event_registration.cgi
+++ www/cgi/set_user_event_registration.cgi
@@ -16,7 +16,7 @@ def main():
     form = cgi.FieldStorage()
     session_id = form.getvalue('session_id')
     event_id = form.getvalue('event_id')
-    registration_status = form.getvalue('registration_status')
+    registration_status = int(form.getvalue('registration_status'))
     user = geardb.get_user_from_session_id(session_id) if session_id else None
 
     print('Content-Type: application/json\n\n')
@@ -31,11 +31,13 @@ def main():
     if registration_status == 1:
         qry = "INSERT INTO event_registration (event_id, user_id) VALUES (%s, %s)"
         cursor.execute(qry, (event_id, user.id))
+        cnx.commit()
         print(json.dumps({'success': 1, 'msg': 'You have successfully registered'}))
     
     elif registration_status == 0:
         qry = "DELETE FROM event_registration WHERE event_id = %s AND user_id = %s"
         cursor.execute(qry, (event_id, user.id))
+        cnx.commit()
         print(json.dumps({'success': 1, 'msg': 'You have successfully unregistered'}))
     
     else:
@@ -44,6 +46,20 @@ def main():
 
     cursor.close()
     cnx.close()
+
+def get_current_count(cursor, event_id):
+    qry = "SELECT "
+
+def get_event_info(cursor, event_id):
+    qry = "SELECT label, max_attendees, waitlist_size FROM event WHERE id = %s"
+    cursor.execute(qry, (event_id,))
+
+    for row in cursor:
+        return {'attendees': 0,
+                'user_attending': 0,
+                'user_waitlisted': 0,
+                'max_attendees': row[2],
+                'waitlist_size': row[3]}
 
     
 if __name__ == '__main__':
--- www/js/workshop.js
+++ www/js/workshop.js
@@ -17,6 +17,7 @@ window.onload=function() {
         dataType:"json",
         success: function(data, textStatus, jqXHR) {
             // Loop through the events on the page and update status of each
+            console.log("Data:");
             console.log(data);
         },
         error: function (jqXHR, textStatus, errorThrown) {
@@ -31,4 +32,68 @@ window.onload=function() {
     //  Register (waitlist)  - blue/primary
     //  Full                 - grey/secondary
     //  Unregister           - red/danger
+    $(document).on('click', "ul.session_actions button", (e) => {
+        e.preventDefault();
+
+        let button = $(e.target);
+        let button_type = $(button).parent().attr("class");
+        let event_id = $(button).data('event-id');
+
+        if (button_type == 'register') {
+            $.ajax({
+                url : './cgi/set_user_event_registration.cgi',
+                type: "POST",
+                data : { 'session_id': session_id,
+                         'event_id': event_id,
+                         'registration_status': 1
+                       },
+                dataType:"json",
+                success: function(data, textStatus, jqXHR) {
+                    if (data.success == 0) {
+                        display_error_bar(data.msg);
+                    } else {
+                        // show only the unregister button now
+                        $("ul#session_" + event_id + " li").hide();
+                        $("ul#session_" + event_id + " li.unregister").show();
+                    }
+                },
+                error: function (jqXHR, textStatus, errorThrown) {
+                    console.log('textStatus= ', textStatus);
+                    console.log('errorThrown= ', errorThrown);
+                    display_error_bar(jqXHR.status + ' ' + errorThrown.name);
+                }
+            });
+
+        } else if (button_type == 'register_wait') {
+
+        } else if (button_type == 'unregister' || button_type == 'unregister_wait') {
+            $.ajax({
+                url : './cgi/set_user_event_registration.cgi',
+                type: "POST",
+                data : { 'session_id': session_id,
+                         'event_id': event_id,
+                         'registration_status': 0
+                       },
+                dataType:"json",
+                success: function(data, textStatus, jqXHR) {
+                    if (data.success == 0) {
+                        display_error_bar(data.msg);
+                    } else {
+                        // show only the unregister button now
+                        $("ul#session_" + event_id + " li").hide();
+
+                        // show register or register_waitlist depending on seats available
+                        $("ul#session_" + event_id + " li.register").show();
+                    }
+                },
+                error: function (jqXHR, textStatus, errorThrown) {
+                    console.log('textStatus= ', textStatus);
+                    console.log('errorThrown= ', errorThrown);
+                    display_error_bar(jqXHR.status + ' ' + errorThrown.name);
+                }
+            });
+        } 
+
+    });
+
 }
