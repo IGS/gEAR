47390f71469e87cb8c80574ee26d1a1954303b89
Joshua Orvis <jorvis@gmail.com>
2023-04-12 15:12:49 -0500

    Test incorporation of Bulma custom styling and SASS integration

---
+++ www/js/common.v2.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', () => {
+    // Add listeners for any elements which have generic close controls
+    (document.querySelectorAll('.notification .delete') || []).forEach(($delete) => {
+        const $notification = $delete.parentNode;
+
+        $delete.addEventListener('click', () => {
+            $notification.parentNode.removeChild($notification);
+        });
+    });
+});
+
