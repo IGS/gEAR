b651ede6fbe88445b8d1df6513b6e3e744b9af75
adkinsrs <sadkins@som.umaryland.edu>
2023-10-04 13:46:19 -0400

    New "facets" widget.  First draft

---
+++ www/js/classes/facets.js
@@ -0,0 +1,263 @@
+const filters = {};
+const sessionId = 'ee95e48d-c512-4083-bf05-ca9f65e2c12a';
+const analysisId = null;
+const datasetId = "3b3410d0-26cd-6d5c-0d99-0a20b8288d26";
+
+// Class for the faceted search widget
+// SAdkins - I let Github Copilot write this class for me (with minor adjustments)
+class FacetWidget {
+    constructor({aggregations, filters, onFilterChange, facetContainer, selectedFacetsContainer, selectedFacetsTags}) {
+        this.aggregations = aggregations || {}; // Counts of all categories (filter)
+        this.filters = filters || {};   // Selected categories and values
+        this.onFilterChange = onFilterChange || {}; // Callback function when a filter is changed
+        this.facetContainer =  facetContainer || document.getElementById('facet_c');
+        this.selectedFacetsContainer = selectedFacetsContainer || document.getElementById('selected_facets');
+        this.selectedFacetsTags = selectedFacetsTags || document.getElementById('selected_facets_tags');
+        this.init();
+    }
+
+    init() {
+        if (this.aggregations.length > 0 ) {
+            this.createFacetWidget();
+        } else {
+            console.warn('No aggregations to display');
+        }
+    }
+
+    createFacetWidget() {
+        const facetWidget = document.createElement('div');
+        facetWidget.id = 'facet-widget';
+        facetWidget.className = 'facet-widget';
+
+        this.facetContainer.append(facetWidget);
+        this.render();
+    }
+
+    updateAggregations(aggregations) {
+        this.aggregations = aggregations;
+        // Update counts
+        for (const filter of this.aggregations) {
+            const escapedFilterName = CSS.escape(filter.name);
+            const filterCount = document.querySelector(`#filter-${escapedFilterName} .filter-count`);
+            filterCount.innerHTML = filter.count;
+
+            for (const item of filter.items) {
+                const escapedItemName = CSS.escape(item.name);
+                // NOTE: For some reason, CSS.escape() does not escape properly for document.getElementById()
+                const filterItemCount = document.querySelector(`#filter-item-${escapedFilterName}-${escapedItemName} .filter-item-count`);
+                const filterItem = document.querySelector(`#filter-item-${escapedFilterName}-${escapedItemName}`);
+                // show item
+                filterItemCount.classList.remove('is-hidden');
+                filterItem.classList.remove('is-hidden');
+                filterItemCount.innerHTML = item.count;
+                // if count is 0, hide item count
+                if (item.count === 0) {
+                    filterItemCount.classList.add('is-hidden');
+                    // if item category is not in the filter category, hide item
+                    if (this.filters.hasOwnProperty(filter.name)) {
+                        // If item is in the filter category, show 0 item count
+                        if (this.filters[filter.name].includes(item.name)) {
+                            filterItemCount.classList.remove('is-hidden');
+                        }
+                    } else {
+                        filterItem.classList.add('is-hidden');
+                    }
+
+                }
+            }
+        }
+    }
+
+    render() {
+        const facetWidget = document.getElementById('facet-widget');
+        facetWidget.innerHTML = '';
+        const facetWidgetContent = document.createElement('div');
+        facetWidgetContent.id = 'facet-widget-content';
+        facetWidgetContent.className = 'facet-widget-content panel';
+        facetWidget.appendChild(facetWidgetContent);
+        this.renderFilters();
+    }
+
+    renderFilters() {
+        const facetWidgetContent = document.getElementById('facet-widget-content');
+        const filters = this.aggregations;
+        for (const filter of filters) {
+            const filterElement = this.createFilterElement(filter);
+            facetWidgetContent.appendChild(filterElement);
+        }
+        // add event listeners for filter toggle
+        const filterHeaders = document.querySelectorAll('.filter-header');
+        for (const filterHeader of filterHeaders) {
+            filterHeader.addEventListener('click', (event) => {
+                this.onFilterHeaderClick(event);
+            });
+        }
+
+    }
+
+    createFilterElement(filter) {
+        const filterElement = document.createElement('div');
+        filterElement.id = `filter-${filter.name}`;
+        filterElement.className = 'filter';
+        filterElement.innerHTML = `
+            <div class="filter-header panel-heading is-clickable">
+                <span class="filter-name has-text-weight-semibold is-underlined">${filter.name}</span>
+                <span class="filter-count tag is-rounded">${filter.count}</span>
+                <span class="filter-toggle-icon icon is-pulled-right">
+                    <i class="mdi mdi-chevron-down"></i>
+                </span>
+            </div>
+        `;
+        const filterContent = this.createFilterContent(filter);
+        filterElement.appendChild(filterContent);
+        return filterElement;
+    }
+
+    createFilterContent(filter) {
+        const filterContent = document.createElement('div');
+        filterContent.id = `filter-content-${filter.name}`;
+        filterContent.className = 'filter-content is-hidden';
+        const filterItems = this.createFilterItems(filter);
+        filterContent.appendChild(filterItems);
+        return filterContent;
+    }
+
+    createFilterItems(filter) {
+        const filterItems = document.createElement('div');
+        filterItems.id = `filter-items-${filter.name}`;
+        filterItems.className = 'filter-items';
+        for (const item of filter.items) {
+            const filterItem = this.createFilterItem(item, filter.name);
+            filterItems.appendChild(filterItem);
+        }
+        return filterItems;
+    }
+
+    createFilterItem(item, filterName) {
+        const filterItem = document.createElement('div');
+        filterItem.id = `filter-item-${filterName}-${item.name}`;   // some item names can be duplicated across different filters
+        filterItem.className = 'filter-item';
+        filterItem.innerHTML = `
+            <label class="panel-block checkbox">
+                <input type="checkbox">
+                <span class="filter-item-name has-text-weight-medium">${item.name}</span>
+                <span class="filter-item-count tag is-rounded">${item.count}</span>
+            </label>
+        `;
+
+        // Since clicking anything in label will trigger the checkbox, we need to add the event listener to the input
+        const inputElt = filterItem.querySelector('input');
+
+        // Check if the filter is already selected
+        if (this.filters.hasOwnProperty(filterName) && this.filters[filterName].includes(item.name)) {
+            inputElt.checked = true;
+        }
+
+        inputElt.addEventListener('click', (event) => {
+            this.onFilterItemClick(item, filterName)
+        });
+        return filterItem;
+    }
+
+    onFilterHeaderClick(event) {
+        const filterHeader = event.currentTarget;
+        const filterContent = filterHeader.nextElementSibling;
+        const filterToggleIcon = filterHeader.querySelector('.filter-toggle-icon');
+        filterContent.classList.toggle('is-hidden');
+
+        // switch toggle icon to up or down
+        if (filterToggleIcon.innerHTML.trim() === '<i class="mdi mdi-chevron-down"></i>') {
+            filterToggleIcon.innerHTML = '<i class="mdi mdi-chevron-up"></i>';
+        } else {
+            filterToggleIcon.innerHTML = '<i class="mdi mdi-chevron-down"></i>';
+        }
+    }
+
+    async onFilterItemClick(item, filterName) {
+        this.toggleFilterItem(item, filterName);
+        this.selectedFacetsContainer.classList.add("is-loading")
+        await this.onFilterChange(this.filters);
+        this.toggleSelectedTag(item.name, filterName);
+        this.selectedFacetsContainer.classList.remove("is-loading")
+
+    }
+
+    toggleFilterItem(item, filterName) {
+        if (this.filters.hasOwnProperty(filterName)) {
+            if (this.filters[filterName].includes(item.name)) {
+                this.filters[filterName] = this.filters[filterName].filter((value) => value !== item.name);
+            } else {
+                this.filters[filterName].push(item.name);
+            }
+        } else {
+            this.filters[filterName] = [item.name];
+        }
+
+        // If all items are selected, remove the filter
+        if (this.filters[filterName].length === this.aggregations.find((value) => value.name === filterName).items.length) {
+            delete this.filters[filterName];
+        }
+
+        // If no filters are selected, remove the filter
+        if (this.filters[filterName].length === 0) {
+            delete this.filters[filterName];
+        }
+    }
+
+    // Add a tag to the selected filter items list
+    toggleSelectedTag(itemName, filterName) {
+        // Append the tag if it doesn't exist, otherwise remove it
+        const tagId = `selected-tag-${filterName}-${itemName}`
+        const selectedTag = document.getElementById(tagId);
+        if (selectedTag === null) {
+            const tag = document.createElement('span');
+            tag.id = tagId;
+            tag.className = 'tag is-light is-primary';
+            tag.textContent = `${filterName}:${itemName}`;
+            this.selectedFacetsTags.appendChild(tag);
+            return;
+        }
+        selectedTag.remove();
+
+    }
+
+}
+
+
+const createFacetWidget = async () => {
+    document.getElementById("selected_facets").classList.add("is-loading")
+
+    const {aggregations} = await fetchAggregations(sessionId, datasetId, analysisId, filters);
+
+    const facetWidget = new FacetWidget({
+        aggregations,
+        filters,
+        onFilterChange: async (filters) => {
+            if (filters) {
+                const {aggregations, total_count:totalCount} = await fetchAggregations(sessionId, datasetId, analysisId, filters);
+                facetWidget.updateAggregations(aggregations);
+            } else {
+                // Save an extra API call
+                facetWidget.updateAggregations(facetWidget.aggregations);
+            }
+        }
+    });
+    document.getElementById("selected_facets").classList.remove("is-loading")
+
+}
+
+const fetchAggregations = async (session_id, dataset_id, analysis_id, filters) => {
+    const payload = {session_id, dataset_id, analysis_id, filters};
+    try {
+        const {data} = await axios.post(`/api/h5ad/${dataset_id}/aggregations`, payload);
+        if (data.hasOwnProperty("success") && data.success < 1) {
+            throw new Error(data?.message || "Could not fetch number of observations for this dataset. Please contact the gEAR team.");
+        }
+        const {aggregations, total_count} = data;
+        return {aggregations, total_count};
+    } catch (error) {
+        logErrorInConsole(error);
+    }
+}
+
+window.onload = () => createFacetWidget();
