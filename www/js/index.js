8d1083cb314d7d1420e23a7283494405491a9b38
Joshua Orvis <jorvis@gmail.com>
2023-12-28 14:19:02 -0600

    Progress commit, switching machines. Work on history table showing proper error state messages

--- www/js/index.js
+++ www/js/index.js
@@ -4,30 +4,33 @@ document.addEventListener('DOMContentLoaded', () => {
 
 const populateUserHistoryTable = async () => {
     const numEntries = 5;
-    try {
-        const data = await apiCallsMixin.fetchUserHistoryEntries(numEntries);
-        const template = document.querySelector('#user-history-row');
+    if (CURRENT_USER) {
+        try {
+            const data = await apiCallsMixin.fetchUserHistoryEntries(numEntries);
+            const template = document.querySelector('#user-history-row');
 
-        for (const entry of data) {
-            const row = template.content.cloneNode(true);
+            for (const entry of data) {
+                const row = template.content.cloneNode(true);
 
-            let formatted_category = entry.entry_category.replaceAll('_', ' ');
-            formatted_category = formatted_category.charAt(0).toUpperCase() + formatted_category.slice(1);
+                let formatted_category = entry.entry_category.replaceAll('_', ' ');
+                formatted_category = formatted_category.charAt(0).toUpperCase() + formatted_category.slice(1);
 
-            row.querySelector('.category').textContent = formatted_category;
-            row.querySelector('.action-label').textContent = entry.label;
-            row.querySelector('.date').textContent = entry.entry_date;
-            row.querySelector('.url').setAttribute('href', entry.url);
+                row.querySelector('.category').textContent = formatted_category;
+                row.querySelector('.action-label').textContent = entry.label;
+                row.querySelector('.date').textContent = entry.entry_date;
+                row.querySelector('.url').setAttribute('href', entry.url);
 
-            document.querySelector('#user-history-table-tbody').appendChild(row);
-        };
-    } catch (error) {
-        console.error(error);
+                document.querySelector('#user-history-table-tbody').appendChild(row);
+            };
+        } catch (error) {
+            console.error(error);
+        }
+    } else {
+        console.log('No user logged in. Clearing user history table.');
+        document.querySelector('#user-history-table-tbody').innerHTML = '';
     }
 }
 
 const handlePageSpecificLoginUIUpdates = async (event) => {
-    if (CURRENT_USER) {
-        populateUserHistoryTable();
-    }
+    populateUserHistoryTable();
 }
