5106a26d0e05ebd1c9d98be0e6ae6bd0dab8b22b
Joshua Orvis <jorvis@gmail.com>
2024-01-14 14:21:53 -0600

    Gene selection and dataset selection on expression page now use widgets

--- www/js/expression.js
+++ www/js/expression.js
@@ -1,8 +1,14 @@
 document.addEventListener('DOMContentLoaded', () => {
     // Set the page header title
     document.querySelector('#page-header-label').textContent = 'Gene Expression Search';
+
+    // Handle passed URL parameters
+
+
 });
 
 const handlePageSpecificLoginUIUpdates = async (event) => {
-    // nothing here yet
+    fetchGeneCartData();
+    fetchDatasetCollections();
 }
+
