9c50ba21a765816ed342625ea69cc3078341d418
Joshua Orvis <jorvis@gmail.com>
2024-02-15 15:20:36 -0600

    Renamed deafness gene annotation plugin files to reflect new page name on which they're used

--- www/plugins/deafness_gene_annotation/expression.js
+++ www/plugins/deafness_gene_annotation/expression.js
@@ -6,25 +6,34 @@ var mgi_data;
 function deafness_plugin_gene_change() {
     var gene_symbol = SELECTED_GENE.text().toLowerCase();
 
-    $("button#deafness_gene_mouse").attr("disabled", true);
-    $("button#deafness_gene_human").attr("disabled", true);
-    $("button#deafness_gene_human_putative").attr("disabled", true);
+    // disable button#deafness_gene_mouse, button#deafness_gene_human, button#deafness_gene_human_putative
+    document.getElementById("deafness_gene_mouse").disabled = true;
+    document.getElementById("deafness_gene_human").disabled = true;
+    document.getElementById("deafness_gene_human_putative").disabled = true;
 
     // reset all
-    $('#deafness_gene_mouse img').attr('src', './img/icons/org-1-outline-64.svg');
-    $('#deafness_gene_human img').attr('src', './img/icons/org-2-outline-64.svg');
-    $('#deafness_gene_human_putative img').attr('src', './img/icons/org-2-unknown-outline-64.svg');
-    $('#deafness_gene_mouse').attr('data-title', 'No data available');
-    $('#deafness_gene_human').attr('data-title', 'No data available');
-    $('#deafness_gene_human_putative').attr('data-title', 'No data available');
-    $('#deafness_popover_phenotypes').html('No data available');
+    document.querySelector('#deafness_gene_mouse img').setAttribute('src', './img/icons/org-1-outline-64.svg');
+    document.querySelector('#deafness_gene_human img').setAttribute('src', './img/icons/org-2-outline-64.svg');
+    document.querySelector('#deafness_gene_human_putative img').setAttribute('src', './img/icons/org-2-unknown-outline-64.svg');
+    document.querySelector('#deafness_gene_mouse').setAttribute('data-title', 'No data available');
+    document.querySelector('#deafness_gene_human').setAttribute('data-title', 'No data available');
+    document.querySelector('#deafness_gene_human_putative').setAttribute('data-title', 'No data available');
+    document.querySelector('#deafness_popover_phenotypes').innerHTML = 'No data available';
 
     var mouse_deafness_links = [];
     
     if (mgi_data.hasOwnProperty(gene_symbol)) {
         mouse_deafness_links = mouse_deafness_links.concat(mgi_data[gene_symbol]['links']);
-        $('#deafness_gene_mouse').attr('data-title', 'Click for links and phenotypes');
+        console.log(mouse_deafness_links);
+        document.querySelector('#deafness_gene_mouse').setAttribute('data-title', 'Click for links and phenotypes');
         
+        const template = document.querySelector('#tmpl_deafness_phenotypes');
+        document.querySelector('#deafness_popover_phenotypes').innerHTML = '';
+
+        for (link of mouse_deafness_links) {
+
+        }
+
         var phenotypes_tmpl = $.templates("#tmpl_deafness_phenotypes");
         var phenotypes_html = phenotypes_tmpl.render(mgi_data[gene_symbol]['phenotypes']);
         $("#deafness_popover_phenotypes").html(phenotypes_html);
@@ -88,14 +97,26 @@ function deafness_plugin_gene_change() {
     });
 }
 
-$(document).ready(function() {
+
     // place the plugin HTML content
-    $("#annotation_plugin_c").html(
-        $("#deafness_gene_annotation_html_c").html()
-    );
+    //$("#annotation_plugin_c").html(
+    //    $("#deafness_gene_annotation_html_c").html()
+    //);
+
+    // First make the external resource links column smaller
+    document.getElementById("annotation-panel-external-links-c").classList.remove("is-9");
+    document.getElementById("annotation-panel-external-links-c").classList.add("is-5");
+    
+    // Add the contents of #deafness_gene_annotation_html_c after the first column under #annotation-panel
+    let oselector_c = document.getElementById('annotation-panel-organism-selector-c');
+    let deafness_gene_annotation_html = document.getElementById('deafness_gene_annotation_html_c').innerHTML;
+    oselector_c.insertAdjacentHTML('afterend', deafness_gene_annotation_html);
+
+    console.log("Element inserted!");
+    
 
     // then destroy the initial placement
-    document.getElementById("deafness_gene_annotation_html_c").remove();
+    //document.getElementById("deafness_gene_annotation_html_c").remove();
 
     // get the data files
     fetch("./plugins/deafness_gene_annotation/impc_data.json")
@@ -174,6 +195,4 @@ $(document).ready(function() {
             console.log('Fetch Error :-S', err);
         });
 
-
-    search_result_postselection_functions.push(deafness_plugin_gene_change);
-});
+    //search_result_postselection_functions.push(deafness_plugin_gene_change);
