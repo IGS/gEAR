c6473791152833cc67380a84a7b9fd30235b9dfe
adkinsrs <sadkins@som.umaryland.edu>
2024-03-21 13:40:32 -0400

    fixing bugs

--- bin/itemize_profile_metrics.py
+++ bin/itemize_profile_metrics.py
@@ -17,7 +17,7 @@ import geardb
 
 def main():
     parser = argparse.ArgumentParser(description="Compute the number of samples in a profile.")
-    parser.add_argument("share_id", "-s",  help="The share ID of the profile.")
+    parser.add_argument("--share_id", "-s",  help="The share ID of the profile.")
     args = parser.parse_args()
 
     # Get the profile
@@ -25,21 +25,21 @@ def main():
     layout.get_members()
 
     profile_name = layout.label
-
-    if args.unique_datasets:
-        dataset_ids = layout.dataset_ids()
+    dataset_ids = layout.dataset_ids()
 
     with open(f"{args.share_id}.tsv", "w") as f:
         f.write("Profile\tDataset\tSpecies\tDatatype\tNumGenes\tNumCells\t")
-        dataset_collection = DatasetCollection()
-        dataset_collection.get_datasets_by_ids(dataset_ids)
+        dataset_collection = geardb.DatasetCollection()
+        dataset_collection.get_by_dataset_ids(dataset_ids)
 
         for dataset in dataset_collection.datasets:
+            title = dataset.title
+            title = f'"{title}"'
             species = dataset.organism
-            dtype = dataset.datatype
+            datatype = dataset.dtype
             (num_samples, num_features) = dataset.get_shape(None, True)
 
-            f.write(f"{profile_name}\t{dataset.name}\t{species}\t{dtype}\t{num_features}\t{num_samples}\n")
+            f.write(f"{profile_name}\t{title}\t{species}\t{datatype}\t{num_features}\t{num_samples}\n")
 
 if __name__ == '__main__':
     main()
