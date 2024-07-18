5a5f6ce18d578129e86c7816a7ea2483cca7804e
Joshua Orvis <jorvis@gmail.com>
2024-07-18 10:11:31 -0500

    Fixed issue where logs were filling with: ImportError: No localization support for language 'eng' from the DB module

--- lib/geardb.py
+++ lib/geardb.py
@@ -875,8 +875,8 @@ class Connection:
         self.mysql_cnx.commit()
 
     def close(self):
-        #! Am getting "ImportError: No localization support for language 'eng'" error here. Put suggested solution in db module but not working
-        self.mysql_cnx.close()
+        if self.mysql_cnx.is_connected():
+            self.mysql_cnx.close()
 
     def get_cursor(self, use_dict=False):
         if use_dict == True:
