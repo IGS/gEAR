4f0a03cd602858539d5dce94381392c9416ab8bc
Joshua Orvis <jorvis@gmail.com>
2024-07-24 12:41:00 -0500

    Gene list selector now always left aligned and dataset collection selector always right. (issue #769)

--- www/js/expression.js
+++ www/js/expression.js
@@ -110,9 +110,6 @@ document.addEventListener('DOMContentLoaded', () => {
                 first_gene.click();
             }
 
-            document.getElementById("dropdown-gene-lists").classList.remove("is-right");
-            document.getElementById("dropdown-dc").classList.remove("is-right");
-
         } catch (error) {
             logErrorInConsole(error);
             return;
