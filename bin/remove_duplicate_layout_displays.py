cd10e4a81fe93b1db53ea6e7a90e729808af053d
adkinsrs <sadkins@som.umaryland.edu>
2024-08-13 13:24:43 -0400

    updates

--- bin/remove_duplicate_layout_displays.py
+++ bin/remove_duplicate_layout_displays.py
@@ -15,9 +15,15 @@ import geardb
 conn = geardb.Connection()
 cursor = conn.get_cursor()
 
+# print row count
+qry = "SELECT COUNT(*) FROM layout_displays"
+cursor.execute(qry)
+row_count = cursor.fetchone()[0]
+print("Row count before deletion: {}".format(row_count))
+
 # https://www.tutorialspoint.com/mysql/mysql-delete-duplicate-records.htm
 qry = """
-DELETE FROM layout_displays ld1
+DELETE ld1 FROM layout_displays ld1
 INNER JOIN layout_displays ld2
 WHERE ld1.layout_id = ld2.layout_id
 AND ld1.display_id = ld2.display_id
@@ -28,3 +34,14 @@ AND ld1.grid_height = ld2.grid_height
 AND ld1.id > ld2.id
 """
 cursor.execute(qry)
+
+cursor.commit()
+
+# print row count
+qry = "SELECT COUNT(*) FROM layout_displays"
+cursor.execute(qry)
+row_count = cursor.fetchone()[0]
+print("Row count after deletion: {}".format(row_count))
+
+cursor.close()
+conn.close()
