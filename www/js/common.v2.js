a811f9ffb5c51f97b064a03b705f44ed0f8d4f81
adkinsrs <sadkins@som.umaryland.edu>
2024-10-16 11:31:50 -0400

    updated UUID to use safer "crypto" built-in

--- www/js/common.v2.js
+++ www/js/common.v2.js
@@ -756,10 +756,7 @@ const resetSteps = (event) => {
 
 // Generates an RFC4122 version 4 compliant UUID
 const uuid = () => {
-    return 'xxxxxxxx-xxxx-xxxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-      const r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
-      return v.toString(16);
-    });
+    return crypto.randomUUID();
 }
 
 for (const jsStep of jsSteps) {
