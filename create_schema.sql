82218ff12551b4d26e2000ad102a60c76e782209
Joshua Orvis <jorvis@gmail.com>
2024-10-23 11:22:36 -0500

    Fixed syntax error in guser table definition

--- create_schema.sql
+++ create_schema.sql
@@ -37,7 +37,7 @@ CREATE TABLE guser (
        date_created   DATETIME DEFAULT CURRENT_TIMESTAMP,
        default_org_id INT NOT NULL DEFAULT 1,
        layout_id VARCHAR(24),
-       is_curator     TINYINT(1) DEFAULT 0
+       is_curator     TINYINT(1) DEFAULT 0,
        FOREIGN KEY fk_guser_doi(default_org_id) REFERENCES organism(id),
        FOREIGN KEY fk_guser_layout(layout_share_id) REFERENCES layout(share_id) ON DELETE CASCADE
 ) ENGINE=INNODB;
