d6c154c7c0e91e0a5fcf01e2ea5ca8eeca07bd1c
adkinsrs <sadkins@som.umaryland.edu>
2024-11-05 15:34:48 -0500

    i hate random empty lines

--- www/js/dataset_explorer.js
+++ www/js/dataset_explorer.js
@@ -85,7 +85,6 @@ class ResultItem {
     }
 
     createListItem() {
-
         const makeRandomString = (length) => {
             let result = '';
             const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
