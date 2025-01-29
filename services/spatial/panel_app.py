c67691bf5c7055e61397897a952726ba3df6a761
adkinsrs <sadkins@som.umaryland.edu>
2025-01-29 09:49:24 -0500

    Y-axis is flipped if image is not present in dataset.

--- services/spatial/panel_app.py
+++ services/spatial/panel_app.py
@@ -82,6 +82,8 @@ class SpatialPlot():
         self.range_x2 = self.spatial_img.shape[1] if self.spatial_img is not None else max(df["spatial1"])
         self.range_y1 = 0 if self.spatial_img is not None else min(df["spatial2"])
         self.range_y2 = self.spatial_img.shape[0] if self.spatial_img is not None else max(df["spatial2"])
+        # if image is provided, the 0,0 (origin) point is the top left corner
+        self.flip = False if self.spatial_img is not None else True
 
     def make_expression_scatter(self):
         df = self.df
@@ -156,7 +158,7 @@ class SpatialPlot():
 
         # adjust domains of all 3 plots, leaving enough space for the colorbar and legend
         self.fig.update_layout(
-            margin=dict(l=20, r=0, t=50, b=0),
+            margin=dict(l=20, r=0, t=50, b=10),
             width=1920 if static_size else None,
             height=1080 if static_size else None,
             plot_bgcolor=plot_bgcolor,
@@ -192,7 +194,11 @@ class SpatialPlot():
 
     def update_axes(self):
         self.fig.update_xaxes(range=[self.range_x1, self.range_x2], title_text="spatial1", showgrid=False, showticklabels=False, ticks="")
-        self.fig.update_yaxes(range=[self.range_y2, self.range_y1], title_text="spatial2", showgrid=False, showticklabels=False, ticks="", title_standoff=0)
+        # flip the y-axis if an image is not present
+        if self.flip:
+            self.fig.update_yaxes(range=[self.range_y1, self.range_y2], title_text="spatial2", showgrid=False, showticklabels=False, ticks="", title_standoff=0)
+        else:
+            self.fig.update_yaxes(range=[self.range_y2, self.range_y1], title_text="spatial2", showgrid=False, showticklabels=False, ticks="", title_standoff=0)
 
     def add_image_trace(self):
         self.convert_image()
