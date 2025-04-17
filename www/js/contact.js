f607ef7858beabe6092037c3228df11e263aabc9
Joshua Orvis <jorvis@gmail.com>
2025-04-17 15:01:44 -0500

    Tagging widget redone to not use JQuery

--- www/js/contact.js
+++ www/js/contact.js
@@ -3,6 +3,12 @@
 const FILE_UPLOAD_LIMIT = 10_485_760; // 10 MB
 let screenshot = null;
 
+let tokenizedTags = [];
+let enteredTags = [];
+const tagField = document.getElementById("tag-field");
+const addTag = document.getElementById("add-tag");
+const tagInput = document.getElementById("tag-input");
+
 window.onload=function() {
     // Set the page title
     document.getElementById('page-header-label').textContent = 'Contact us';
@@ -13,32 +19,83 @@ window.onload=function() {
     //TODO prepopulate email field if user is logged in
 };
 
-// Generate a list of existing tags from database
-// Autocompletes in TAG input as twitter-like tokens
-function get_tag_list() {
-    $.get("./cgi/get_tag_list.cgi", (data) => {
-      if (data.success == 1) {
-        // put all tags into a list
-        const tokenized_tags = [];
-        for (i = 0; i < data.tags.length; i++) {
-          tokenized_tags.push(data.tags[i].label);
-        }
+addTag.addEventListener("click", () => {
+    if (tagInput.value !== "") {
+      createTag(tagInput.value);
+    }
+    tagInput.value = "";
+});
   
-        // Create dropdown of tags for autocomplete
-        $("#comment_tag").select2({
-          allowClear: true,
-          data: tokenized_tags,
-          placeholder: "Examples: mouse sox2 upload issue",
-          tags: true,
-          tokenSeparators: [",", " "]
-        })
-  
-      } else {
-        console.error("Handle a failed report from the CGI");
-      }
-    }).fail(() => {
-      console.error("Something broke. Unable to generate tag list");
+tagInput.addEventListener("keyup", (event) => {
+    if ((event.keyCode === 13) && (tagInput.value !== "")) {
+      createTag(tagInput.value);
+      tagInput.value = "";
+    }
+});
+
+/* Credit (adapted from)
+    https://codepen.io/yanniskatsaros/pen/ZEQByPj
+*/
+function createTag(message) {
+    const controlDiv = document.createElement("div");
+    controlDiv.classList.add("control");
+    
+    const tags = document.createElement("div");
+    tags.classList.add("tags", "has-addons");
+    
+    const tagContent = document.createElement("a");
+    tagContent.classList.add("tag", "is-link");
+    tagContent.innerText = message;
+    
+    const tagDelete = document.createElement("a");
+    tagDelete.classList.add("tag", "is-delete");
+    tagDelete.addEventListener("click", (event) => {
+      tagField.removeChild(controlDiv);
     });
+    
+    // finally nest all the tags together
+    tags.appendChild(tagContent);
+    tags.appendChild(tagDelete);
+    controlDiv.appendChild(tags);
+    tagField.appendChild(controlDiv);
+
+    enteredTags.push(message);
+    
+    /* Each adds this HTML to the container:
+    <div class="tags has-addons">
+        <a class="tag is-link">${message}</a>
+        <a class="tag is-delete"></a>
+    </div>
+    */
+}
+
+// Generate a list of existing tags from database
+function get_tag_list() {
+
+    fetch("./cgi/get_tag_list.cgi")
+      .then(response => response.json())
+      .then(data => {
+        if (data.success == 1) {
+          // put all tags into a list
+          tokenizedTags = data.tags.map(tag => tag.label);
+
+          // Create dropdown of tags for autocomplete
+          /*
+          const commentTag = document.getElementById("comment_tag");
+          commentTag.setAttribute("placeholder", "Examples: mouse sox2 upload issue");
+          commentTag.addEventListener("input", (event) => {
+            const inputValue = event.target.value;
+            const suggestions = tokenized_tags.filter(tag => tag.includes(inputValue));
+            // Logic to display suggestions can be added here
+          });
+          */
+        } else {
+          console.error("Handle a failed report from the CGI");
+        }
+      })
+      .catch(() => {
+        console.error("Something broke. Unable to generate tag list");
+      });
   } //end get_tag_list()
 
 const handlePageSpecificLoginUIUpdates = async (event) => {
