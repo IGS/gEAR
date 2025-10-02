fd2117e8075749e8667624eff8f238a439ba765a
adkinsrs <sadkins@som.umaryland.edu>
2025-10-02 15:16:00 -0400

    Specify version for projectR package installation in install_packages.R

--- docker/install_packages.R
+++ docker/install_packages.R
@@ -10,7 +10,7 @@ library(devtools)   # for install_github
 tryCatch( {
     install.packages("reticulate", repos = "https://cloud.r-project.org/", ask = FALSE) # Sanity check with rpy2
     install_github("ctlab/fgsea")   # needed for projectR
-    install_github("genesofeve/projectR")
+    install_github("genesofeve/projectR@d3dd79e2b14172a9561059d58462c97f0a78d4c8")  # version 1.23.2
     BiocManager::install("biomaRt", ask=FALSE) # version 2.60.0
     install_github("CHuanSite/SJD")
     }, error = function(e) {
