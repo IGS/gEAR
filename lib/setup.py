e8b051e69d9dd1621184bd3453a043e2785b0d55
adkinsrs <sadkins@som.umaryland.edu>
2026-03-24 15:43:39 -0400

    Implement Gosling Upload Consumer with Docker and systemd support

---
+++ lib/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="gear",
+    version="1.0.0",
+    packages=find_packages(),
+    description="gEAR library utilities",
+    python_requires=">=3.10",
+)
